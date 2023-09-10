b5194372dea1c20d6da9c85c7e87fc783bce0816
aroxu <30624945+aroxu@users.noreply.github.com>
2023-09-10 20:11:40 +0900

    :art: Remove . in fsPath

--- rest/files.go
+++ rest/files.go
@@ -20,7 +20,7 @@ func GETFiles(c *gin.Context) {
 	fsPath := strings.Replace(requestURL, "/files", ".", 1)
 	_executablePath, err := os.Executable()
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "detail": err.Error(), "path": fsPath})
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "detail": err.Error(), "path": strings.Replace(fsPath, ".", "", 1)})
 		return
 	}
 	_absFSPath := filepath.Join(filepath.Dir(_executablePath), fsPath)
@@ -34,27 +34,27 @@ func GETFiles(c *gin.Context) {
 	}
 
 	if filepath.ToSlash(_executablePath) == filepath.ToSlash(_targetFilePath) {
-		c.JSON(http.StatusNotAcceptable, gin.H{"status": "failed", "detail": "cannot read launcher itself.", "path": fsPath})
+		c.JSON(http.StatusNotAcceptable, gin.H{"status": "failed", "detail": "cannot read launcher itself.", "path": strings.Replace(fsPath, ".", "", 1)})
 		return
 	}
 
 	// then, check if the requested path is exists
 	if _, err := os.Stat(_targetFilePath); os.IsNotExist(err) {
-		c.JSON(http.StatusNotFound, gin.H{"status": "failed", "detail": err.Error(), "path": fsPath})
+		c.JSON(http.StatusNotFound, gin.H{"status": "failed", "detail": err.Error(), "path": strings.Replace(fsPath, ".", "", 1)})
 		return
 	}
 
 	// then check if the requested path is a directory
 	fileInfo, err := os.Stat(_targetFilePath)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "detail": err.Error(), "path": fsPath})
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "detail": err.Error(), "path": strings.Replace(fsPath, ".", "", 1)})
 		return
 	}
 
 	if fileInfo.IsDir() {
 		files, err := os.ReadDir(_targetFilePath)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "detail": err.Error(), "path": fsPath})
+			c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "detail": err.Error(), "path": strings.Replace(fsPath, ".", "", 1)})
 			return
 		}
 		dirStatus := []DirectoryStatus{}
@@ -63,7 +63,7 @@ func GETFiles(c *gin.Context) {
 				dirStatus = append(dirStatus, DirectoryStatus{FileName: file.Name(), IsDirectory: file.IsDir()})
 			}
 		}
-		c.JSON(http.StatusOK, gin.H{"status": "ok", "content": dirStatus, "path": fsPath})
+		c.JSON(http.StatusOK, gin.H{"status": "ok", "content": dirStatus, "path": strings.Replace(fsPath, ".", "", 1)})
 	} else {
 		c.Status(http.StatusOK)
 		c.File(_targetFilePath)
@@ -76,7 +76,7 @@ func DELETEFiles(c *gin.Context) {
 	fsPath := strings.Replace(requestURL, "/files", ".", 1)
 	_executablePath, err := os.Executable()
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "detail": err.Error(), "path": fsPath})
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "detail": err.Error(), "path": strings.Replace(fsPath, ".", "", 1)})
 		return
 	}
 	_absFSPath := filepath.Join(filepath.Dir(_executablePath), fsPath)
@@ -90,22 +90,22 @@ func DELETEFiles(c *gin.Context) {
 	}
 
 	if filepath.ToSlash(_executablePath) == filepath.ToSlash(_targetFilePath) {
-		c.JSON(http.StatusNotAcceptable, gin.H{"status": "failed", "detail": "cannot delete launcher itself.", "path": fsPath})
+		c.JSON(http.StatusNotAcceptable, gin.H{"status": "failed", "detail": "cannot delete launcher itself.", "path": strings.Replace(fsPath, ".", "", 1)})
 		return
 	}
 
 	// then, check if the requested path is exists
 	if _, err := os.Stat(_targetFilePath); os.IsNotExist(err) {
-		c.JSON(http.StatusNotFound, gin.H{"status": "failed", "detail": err.Error(), "path": fsPath})
+		c.JSON(http.StatusNotFound, gin.H{"status": "failed", "detail": err.Error(), "path": strings.Replace(fsPath, ".", "", 1)})
 		return
 	}
 
 	// then delete requested path
 	err = os.RemoveAll(_targetFilePath)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "detail": err.Error(), "path": fsPath})
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "failed", "detail": err.Error(), "path": strings.Replace(fsPath, ".", "", 1)})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"status": "ok", "path": fsPath})
+	c.JSON(http.StatusOK, gin.H{"status": "ok", "path": strings.Replace(fsPath, ".", "", 1)})
 }
